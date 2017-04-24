023ef9d019b5dd8626aadae3895ce5a07f813eca
derekagilbert <dgibbbs17@gmail.com>
2017-04-24 16:22:39 -0600

    Implmented authorization engine for portfolio items and blogs

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -2,14 +2,16 @@ module CurrentUserConcern
 	extend ActiveSupport::Concern
 
   	def current_user
-  	  super || geust_user
+  	  super || guest_user
   	end
 
-  	def geust_user
-  		OpenStruct.new(name: "Guest User", 
-  			           first_name: "Guest", 
-  			           last_name: "Guest User",
-  			           email: "guest@fakemail.com"
-  			           )
+  	def guest_user
+      guest = GuestUser.new
+      guest.name = "Guest User"
+      guest.first_name = "Guest"
+      guest.last_name = "User"
+      guest.email = "guest@example.com"
+      guest
+  		
   	end
   end
