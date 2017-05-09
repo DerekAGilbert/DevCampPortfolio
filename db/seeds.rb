ae9ae649048cce16e72ab7eea43287b06252af7a
derekagilbert <dgibbbs17@gmail.com>
2017-05-09 00:02:51 -0600

    fixed various bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,24 @@
+
+User.create!(
+  email: "dgibbbs17@gmail.com",
+  password: "pacsun25",
+  password_confirmation: "pacsun25",
+  name: "Admin User",
+  roles: "site_admin"
+)
+
+puts "1 Admin user created"
+
+User.create!(
+  email: "test2@test.com",
+  password: "asdfasdf",
+  password_confirmation: "asdfasdf",
+  name: "Regular User",
+)
+
+puts "1 regular user created"
+
+
 3.times do |topic|
 	Topic.create!(
       title: "Topic #{topic}"
